4f89d0509885053fae5222949ad3f515b2042b7b
romariom99 <romariomarimon21@gmail.com>
2018-10-12 20:19:58 -0500

    Parcial

--- create_dockers.sh
+++ create_dockers.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Lanzando servidor web..."
-docker run -d -P --name server01 -p 2221:22 -p 8000:80 $1
+docker run -d -P --name server01 -p 2221:22 -p 80:80 $1
 echo "Servidor desplegado."
 echo
 echo "----------------------"
